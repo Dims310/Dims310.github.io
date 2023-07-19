5287bc79415cb8e61cddc4ab874a7b250854fae4
Ardimas <94347837+Dims310@users.noreply.github.com>
2023-07-19 09:25:20 +0700

    update api query

--- resources/github-api.js
+++ resources/github-api.js
@@ -8,23 +8,25 @@ async function getRepos(){
     const api = await fetch(repos)
     const data = await api.json()
     
-    Object.keys(data).forEach(function (i){
-        var dataCardRepos = `
-            <div class="col">
-                <div class="card h-100">
-                    <div class="card-body body">
-                        <h5 class="card-title">${data[i].name}</h5>
-                        <p class="card-text">${data[i].description}.</p>
-                    </div>
-                    <div class="card-footer footer">
-                        <a href="${data[i].html_url}" class="btn btn-primary">Go to repository <i class="ri-external-link-line"></i></a>
+    for (let i=1; i < Object.keys(data).length; i++){
+        if (data[i].description !== null){
+            var dataCardRepos = `
+                <div class="col">
+                    <div class="card h-100">
+                        <div class="card-body body">
+                            <h5 class="card-title">${data[i].name}</h5>
+                            <p class="card-text">${data[i].description}.</p>
+                        </div>
+                        <div class="card-footer footer">
+                            <a href="${data[i].html_url}" class="btn btn-primary">Go to repository <i class="ri-external-link-line"></i></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        `
+            `
 
-        document.getElementById('reposCard').innerHTML += dataCardRepos
-    })
+            document.getElementById('reposCard').innerHTML += dataCardRepos
+        }
+    }
 }
 
 getRepos()
