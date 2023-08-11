0adb4f11df8ec93a139fd8aec74e6d6f15d3d242
Ardimas <94347837+Dims310@users.noreply.github.com>
2023-08-11 16:01:16 +0700

    add dark-mode feature

--- resources/github-api.js
+++ resources/github-api.js
@@ -11,14 +11,14 @@ async function getRepos(){
     for (let i=1; i < Object.keys(data).length; i++){
         if (data[i].description !== null){
             var dataCardRepos = `
-                <div class="col">
+                <div class="col-lg-4 mt-3 reposCard">
                     <div class="card h-100">
                         <div class="card-body body">
                             <h5 class="card-title">${data[i].name}</h5>
                             <p class="card-text">${data[i].description}.</p>
                         </div>
-                        <div class="card-footer footer">
-                            <a href="${data[i].html_url}" class="btn btn-primary">Go to repository <i class="ri-external-link-line"></i></a>
+                        <div class="card-footer">
+                            <a href="${data[i].html_url}"><button class="btn">Go to repository <i class="ri-external-link-line"></i></button></a>
                         </div>
                     </div>
                 </div>
@@ -30,3 +30,8 @@ async function getRepos(){
 }
 
 getRepos()
+
+function darkMode() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
