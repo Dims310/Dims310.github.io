5524464e0bbdc596c732c0e6f20cf2edd3cecfb6
Ardimas <>
2024-09-13 08:36:18 +0700

    optimizing cdn usage, update ui, remove unused css property, dims310

--- resources/github-api.js
+++ resources/github-api.js
@@ -1,35 +1,37 @@
-// fetch('https://api.github.com/users/Dims310/repos')
-//     .then((result) => result.json())
-//     .then((data) => console.log(data))
+$(document).ready(function() {
+    const apiRepository = "https://api.github.com/users/Dims310/repos";
 
-const repos = "https://api.github.com/users/Dims310/repos"
+    $.getJSON( apiRepository, function() {})
+        .done(function(data) {
+            console.log(data);
+            
+            
+            for (let i=2; i < data.length; i+=2) {
 
-async function getRepos(){
-    const api = await fetch(repos)
-    const data = await api.json()
-    
-    for (let i=1; i < Object.keys(data).length; i++){
-        if (data[i].description !== null){
-            var dataCardRepos = `
-                <div class="col-lg-4 mt-3 reposCard">
-                    <div class="card h-100">
-                        <div class="card-body body">
-                            <h5 class="card-title">${data[i].name}</h5>
-                            <p class="card-text">${data[i].description}.</p>
-                        </div>
-                        <div class="card-footer">
-                            <a href="${data[i].html_url}"><button class="btn">Go to repository <i class="ri-external-link-line"></i></button></a>
+                var dataCardRepos = `
+                    <div class="col-lg-4 mt-3 reposCard">
+                        <div class="card h-100">
+                            <div class="card-header">
+                                ${data[i].language}
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">${data[i].name}</h5>
+                                <p class="card-text">${data[i].description}.</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="${data[i].html_url}"><button class="btn"><span id="linked-repo">Go to repository</span> <i class="ri-external-link-line"></i></button></a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            `
+                `
 
-            document.getElementById('reposCard').innerHTML += dataCardRepos
-        }
-    }
-}
-
-getRepos()
+                $("#reposCard").append(dataCardRepos);
+            }
+        })
+        .fail(function() {
+            console.log("Error, cannot reach the repositories");
+        })
+})
 
 function darkMode() {
    var element = document.body;
